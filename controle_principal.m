717edc4aae40612d2e3bf8e67063bd623bb74fe7
vrogly <vsu.forward.unit@gmail.com>
2024-05-28 15:36:47 +0000

    Added full LQG with noise and disturbances

--- controle_principal.m
+++ controle_principal.m
@@ -1,9 +1,10 @@
 clc;clear;
 close all
+set(groot,'defaultAxesFontSize',16)
 
 disp("Running")
 
-syms T1 T2 T3 T4 T5 T6 T_ar mdot cH2O Qdotger rho Delta_P_R Delta_P_H2O V_motor V_cell U_A F
+syms T1 T2 T3 T4 T5 T6 T_ar mdot cH2O Qdotger rho Delta_P_R Delta_P_H2O V_motor V_cell U_A F;
 
 dTi = [(mdot*cH2O*(T6-T1) + Qdotger +(mdot /rho)*Delta_P_H2O ) / (rho * V_motor * cH2O), %
     (-F*U_A*((T1-T2)/(log((T1-T_ar)/(T2-T_ar)))) + mdot*cH2O*(T1-T2) -(mdot/rho)*Delta_P_R ) / (rho * V_cell * cH2O),%
@@ -11,23 +12,22 @@ dTi = [(mdot*cH2O*(T6-T1) + Qdotger +(mdot /rho)*Delta_P_H2O ) / (rho * V_motor 
     (-F*U_A*((T3-T4)/(log((T3-T_ar)/(T4-T_ar)))) + mdot*cH2O*(T3-T4) -(mdot/rho)*Delta_P_R ) / (rho * V_cell * cH2O),
     (-F*U_A*((T4-T5)/(log((T4-T_ar)/(T5-T_ar)))) + mdot*cH2O*(T4-T5) -(mdot/rho)*Delta_P_R ) / (rho * V_cell * cH2O),
     (-F*U_A*((T5-T6)/(log((T5-T_ar)/(T6-T_ar)))) + mdot*cH2O*(T5-T6) -(mdot/rho)*Delta_P_R ) / (rho * V_cell * cH2O),
-    ]
+    ];
 dT_pure = matlabFunction(dTi);
 
-Tarr = [T1,T2,T3,T4,T5,T6]
-Tnames = ['T_1','T_2','T_3','T_4','T_5','T_6']
+Tarr = [T1,T2,T3,T4,T5,T6];
+Tnames = ['T_1','T_2','T_3','T_4','T_5','T_6'];
 
-nT = max(size(Tarr))
-nCells = nT-1 % We have one more T (surface) than cells
+nT = max(size(Tarr));
+nCells = nT-1; % We have one more T (surface) than cells
 
 
-u_params = [U_A] % Note! You will have do do manual changes if you increase params here by adding more u_UA etc functions
-fluctating_params = [Qdotger, mdot,Delta_P_H2O,T_ar]
+u_params = [U_A]; % Note! You will have do do manual changes if you increase params here by adding more u_UA etc functions
+fluctating_params = [Qdotger, mdot,Delta_P_H2O,T_ar];
 
-A = jacobian(dTi, Tarr)
-%B_full = jacobian(dTi, [Qdotger,U_A,mdot,Delta_P_H2O, T_ar])
-B = jacobian(dTi, u_params)
-E = jacobian(dTi, fluctating_params)
+A = jacobian(dTi, Tarr);
+B = jacobian(dTi, u_params);
+E = jacobian(dTi, fluctating_params);
 
 latexify("\frac{d}{dt}\vec T =",dTi)
 latexify("A=",A)
@@ -35,74 +35,58 @@ latexify("B=",B)
 
 % y independent of x after a substitution, i.e. we cant update the value again
 
-LiterToM3 = 1000
-F = 0.9
-cH2O = 4186 % J/kgK
-rho = 1*LiterToM3 % kg/L
-mdot = 25/60 % kg/s
-Qdotger = 6000 % W
+LiterToM3 = 1000;
+F = 0.9;
+cH2O = 4186; % J/kgK
+rho = 1*LiterToM3; % kg/L
+mdot = 25/60; % kg/s
+Qdotger = 6000; % W
 
-Delta_P_H2O = 35000 % Pa
-Delta_P_R = Delta_P_H2O/nCells % Pa
+Delta_P_H2O = 35000; % Pa
+Delta_P_R = Delta_P_H2O/nCells; % Pa
 
-V_motor = 1.5/LiterToM3 % L
-V_cell = V_motor/nCells
+V_motor = 1.5/LiterToM3; % L
+V_cell = V_motor/nCells;
 
-F = 0.9
-T_ar = 298 % K
-U_A = 100/nCells % W/K
+F = 0.9;
+T_ar = 298; % K
+U_A = 100/nCells; % W/K
 
-Qdotger_fluc = 1500 % W
-mdot_fluc = 5/60 % kg/s
-Delta_P_H20_fluc = 5000 % Pa
-T_ar_fluc = 10 % C
-
-fluc_arr = [Qdotger_fluc, mdot_fluc, Delta_P_H20_fluc, T_ar_fluc]
-
-vd = diag(fluc_arr./fluctating_params) % really important that these are in right order
-
-vd = subs(vd)
 dTi = subs(dTi);
 
-% Equilibrium conditions
+% Simultion used for comparision
+dt = 0.04;
+tSimulation = 0:dt:40;
+TarrSimulation = [370;369;368;367;366;365];
 
-%dT_handle = matlabFunction(dTi);
-%dT_lambda = @(t,T)dT_handle(T(1),T(2),T(3),T(4),T(5),T(6))
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Simultion comparision
-tSimulation = 0:0.1:35;
-TarrSimulation = [370;369;368;367;366;365]
-
-
-eq_sol = solve(dTi)
+eq_sol = solve(dTi);
 T1 = eq_sol.T1;
 T2 = eq_sol.T2;
 T3 = eq_sol.T3;
 T4 = eq_sol.T4;
 T5 = eq_sol.T5;
 T6 = eq_sol.T6;
-T_equilibrium = [T1;T2;T3;T4;T5;T6]
+T_equilibrium = [T1;T2;T3;T4;T5;T6];
 
 A = double(subs(A));
 B = double(subs(B));
 E = double(subs(E));
 
-
 C_full_observe = eye(size(A)); % Matriz de identidade
-C = zeros([2, nT]); % We only measure 2 values
+
+nMeasuresC = 2;
+C = zeros([nMeasuresC, nT]); % We only measure 2 values
 C(1) = 1; % We can only measure edge temperatures
 C(end) = C(1);
-%C(2,2 ) = 1
+for nn = 2:nMeasuresC-1
+    C(nn,nn) = 1;
+end
 
 D_full_observe = zeros([size(C_full_observe,1),size(B,2)]); % Matriz de zeros
 D = zeros([size(C,1),size(B,2)]); % Matriz de zeros
 
-
 % Definindo o sistema
-sys = ss(A, B, C_full_observe, D_full_observe)
-
-%%
+sys_ol = ss(A, B, C_full_observe, D_full_observe)
 
 % Obtendo os polos
 polos = eig(A);
@@ -120,269 +104,187 @@ for IU = 1:size(B,2)
 end
 
 % Plotando o diagrama de Bode
-%figure;
-%bode(sys);
-%title('Diagrama de Bode');
+figure;
+bode(sys_ol);
+title('Diagrama de Bode');
 
 %%
 
 %%%% REGULADOR LINEAR QUADRÁTICO %%%%
 
 % Definindo as matrizes de peso Q e R
-Q = diag([1,0,0,0,0,1])%
+Q = diag([1,0,0,0,0,1]);%
 % Did not help to put 0.1 or 0.5 instead of zeros, only made worse 
-% eye(size(A)); % Matriz de peso para os estados
 R = 0.01; % Peso para a entrada, %% Make it expensive to change fan speed
 
 % Calculando o ganho do controlador LQR
-Klq = lqr(A, B, Q, R);
+Klqr = lqr(A, B, Q, R);
 
 % Sistema em malha fechada com controlador LQR
-A_cl1 = A - B * Klq;
-sys_cl1 = ss(A_cl1, B, C_full_observe, D_full_observe);
+A_cl_lqr = A - B * Klqr;
+
+sys_cl_lqr = ss(A_cl_lqr, B, C_full_observe, D_full_observe);
+Co = ctrb(sys_cl_lqr);
+rank_co = rank(Co);
 
 % Exibindo os polos
-polos_lqr = eig(A_cl1);
+polos_lqr = eig(A_cl_lqr);
 disp('Polos do sistema com controlador LQR:');
 disp(polos_lqr);
+
+%% Compare open loop to closed loop with LQR
+
+u_UA_malha_aberta = @(T)U_A;
+[t,y_malha_aberta] = ode45(@(t,T)dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T(1),T(2),T(3),T(4),T(5),T(6),T_ar,u_UA_malha_aberta(T),V_cell,V_motor,cH2O,mdot,rho),tSimulation,TarrSimulation);
+
+u_UA_LQR = @(T)double(U_A-Klqr*(T-T_equilibrium));
+[t,y_LQR] = ode45(@(t,T)dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T(1),T(2),T(3),T(4),T(5),T(6),T_ar,u_UA_LQR(T),V_cell,V_motor,cH2O,mdot,rho),tSimulation,TarrSimulation);
+
+figure;
+title("Controlling initial conditions to desired state")
+plot(tSimulation,y_malha_aberta,'r');
+hold on;
+plot(tSimulation,y_LQR,'b');
+hold on;
+legend("Malha aberta","","","","","","","LQR")
+ylabel('T_1, T_2, T_3, T_4, T_5, T_6 (K)');
+xlabel('Time (s)');
+
+u_LQR_history = [];
+for i = 1:size(y_LQR,1)
+    u_LQR_history = [u_LQR_history nCells * u_UA_LQR(transpose(y_LQR(i,:)))];
+end    
+figure;
+plot(tSimulation, nCells*U_A*ones(size(tSimulation)),'r');hold on
+plot(tSimulation,u_LQR_history,'b')
+ylabel('U_A (W/K)');
+xlabel('Time (s)');
+legend("Malha aberta","LQR");
+
+
+
 
 
 %% Pole Placement
 
 % Uses LQR poles and tries to vary them slightly to manually
 % decide if there are more suitable placements
+% plot the control u over time to see that it does not exceed any
+% specifications
 
-polesFactorArr = [0.7, 0.9,1, 1.1, 1.3]
-%polesFactorArr = [0.3]
+polesFactorArr = [0.7, 0.9, 1, 1.1, 1.3];
+T_pole_history = figure; 
+u_pole_history = figure;
 
-figure;
 for poleFactor = 1:size(polesFactorArr,2)
     desired_poles = polos_lqr * polesFactorArr(poleFactor)
     Kp = place(A, B, desired_poles);
     A_poleTemp = A - B * Kp;
     disp(eig(A_poleTemp))
-    %sys_poleTemp = ss(A_poleTemp, B, C_full_observe, D_full_observe);
-    %figure;
-
-    %title(append('Step response to changed U_A, Pole placement: LQR \times' ,num2str(polesFactorArr(poleFactor))));
-
     
+    % Here we could build and simulate a state space model like
+    % sys_poleTemp = ss(A_poleTemp, B, C_full_observe, D_full_observe);
+    % But instead we simulate full non-linear dynamics and update the
+    % control vairable using our linear model
+
     u_UA_KP = @(T)double(U_A-Kp*(T-T_equilibrium));
     [t,y_KP] = ode45(@(t,T)dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T(1),T(2),T(3),T(4),T(5),T(6),T_ar,u_UA_KP(T),V_cell,V_motor,cH2O,mdot,rho),tSimulation,TarrSimulation);    
     
-    %plot(tSimulation,y_KP(:,1))
+    figure(T_pole_history);
+    plot(tSimulation,y_KP(:,1)); hold on;
     
     u_KP_history = [];
     for i = 1:size(y_KP,1)
         u_KP_history = [u_KP_history nCells * u_UA_KP(transpose(y_KP(i,:)))];
     end
-    plot(tSimulation,u_KP_history)
-
-
-    hold on
+    figure(u_pole_history);
+    plot(tSimulation,u_KP_history); hold on;
 end
-%ylabel('T_1 (K)');
-ylabel('U_A (-)');
+figure(T_pole_history);
+title("Controlling initial conditions to desired state")
+ylabel('T_1 (K)');
 xlabel('Time (s)');
-
 legend('0.7', '0.9', 'LQR', '1.1', '1.3')
 
-%% LQE
+
+figure(u_pole_history);
+title("Controlling initial conditions to desired state")
+ylabel('U_A (W/K)');
+xlabel('Time (s)');
+legend('0.7', '0.9', 'LQR', '1.1', '1.3')
 
 
 
-sys_cl_k = ss(A, B, C, D);
-sys_cl_k.InputName = 'U_A';
-sys_cl_k.OutputName = 'T1-6';
+%% Luenberger
 
-%[L,P,E] = lqe(A,Vd,C,Vd,Vn)
-%Kf = (lqr(A',C',Vd,Vn))'
+% @M, here we need realistic values, we can discuss
+Vd = 0.1*eye([6,6]);
+Vn = 0.1*eye([2,2]);
 
-Q = 10;
-R_kal = 10;
-N = 0; % No correlation
-
-[kalmf,L,P] = kalman(sys_cl_k,Q,R_kal,N);
-size(kalmf)
-
-kalmf.InputGroup
-
-%figure;
-%disp("not using kalman filter")
-%step(sys_cl_k,t,respOpt)
-%title('Stepresponse to changed U_A, Kalman');
+rng(1,"twister");
+disturbance_noise = sqrt(Vd)*randn(6,size(tSimulation,2));
+measure_noise = sqrt(Vn)*randn(2,size(tSimulation,2));
 
 
-% TODO add simulation with kalman filter, basically what we have here
-% https://www.mathworks.com/help/control/ug/kalman-filtering.html
-% Also, test controlability
-% Pole placement just change LQR with 10%
-% Plot all poles
+% Create observer, LQE, with LQR regulator
+sys_observer = ss(A_cl_lqr, B, C, D);
+L_lqr_obs = (lqr(A_cl_lqr',C',Vd,Vn))';
+eig(A_cl_lqr-L_lqr_obs*C);
 
+T_current = TarrSimulation;
+T_current_lqr = T_current;
+% Use the order of magnitude as an initial guess,
+% the initial guess heavily effects the convergence
+% to the correct value
+T_approx_prev = T_equilibrium; 
 
+Tsol = [];
+Tsol_approx = [];
+Tlqr = [];
+Tmeasures = [];
 
+Tsol_approx = [Tsol_approx T_approx_prev];
+Tsol = [Tsol T_current];
+Tlqr = [Tlqr T_current_lqr];
+Tmeasures = [Tmeasures C*(T_current)];
 
-%% Simulations performed here tambem
-T_equilibrium
+% We solve for full non linear dynamics but use linear control
+for time = 2:size(tSimulation,2)
+    T_measured = C*(T_current)+measure_noise(time);
+    T_approx_current = T_approx_prev +  dt * (A*(T_approx_prev-T_equilibrium) + B*(u_UA_LQR(T_approx_prev)-U_A) + L_lqr_obs*((T_measured-C*T_equilibrium) - C*(T_approx_prev-T_equilibrium)));
+    T_approx_prev = T_approx_current;
 
-u_UA_malha_aberta = @(T)U_A;
-[t,y_malha_aberta] = ode45(@(t,T)dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T(1),T(2),T(3),T(4),T(5),T(6),T_ar,u_UA_malha_aberta(T),V_cell,V_motor,cH2O,mdot,rho),tSimulation,TarrSimulation);
-y_malha_aberta(end,:)
+    % Euler's method to solve differential equations (instead of ode45 as
+    % used for the same task in the rest of the code. This is beacuse the
+    % Luenberger must know previous state and we want to manually add noise
+    % and still simulate the non linear system.
+    T_current = T_current + dt*(disturbance_noise(time) + ...
+        dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T_current(1),T_current(2),T_current(3),T_current(4),T_current(5),T_current(6),T_ar,u_UA_LQR(T_approx_current),V_cell,V_motor,cH2O,mdot,rho));
+    T_current_lqr = T_current_lqr + dt*(disturbance_noise(time) + ...
+        dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T_current_lqr(1),T_current_lqr(2),T_current_lqr(3),T_current_lqr(4),T_current_lqr(5),T_current_lqr(6),T_ar,u_UA_LQR(T_current_lqr),V_cell,V_motor,cH2O,mdot,rho));
 
-u_UA_LQR = @(T)double(U_A-Klq*(T-T_equilibrium));
-[t,y_LQR] = ode45(@(t,T)dT_pure(Delta_P_R,Delta_P_H2O,F,Qdotger,T(1),T(2),T(3),T(4),T(5),T(6),T_ar,u_UA_LQR(T),V_cell,V_motor,cH2O,mdot,rho),tSimulation,TarrSimulation);
-y_LQR(end,:)
-
-
-figure;
-plot(tSimulation,y_malha_aberta,'r')
-hold on
-plot(tSimulation,y_LQR,'b')
-hold on
-
-%%
-u_LQR_history = [];
-for i = 1:size(y_LQR,1)
-    u_LQR_history = [u_LQR_history nCells * u_UA_LQR(transpose(y_LQR(i,:)))];
-end    
-figure;
-plot(tSimulation,u_LQR_history)
-
-
-
-
-
-
-
-
-%% Simulation part IGNORE THIS
-
-
-%{
-% Simular a resposta ao degrau do sistema
-[ylq, t, xlq] = lsim(sys_cl1, u, t, x0);
-
-% Plotar a resposta do sistema
-figure;
-plot(t, ylq)
-xlabel('Tempo (s)')
-ylabel('Saída do sistema (K)')
-legend(Tnames)
-title('Resposta do Sistema com Controlador LQR')
-grid on
-
-% Comparacao entre LQR e Alocacao de polos 
-figure;
-plot(t, yp, '--', t, ylq);
-xlabel('Tempo (s)');
-ylabel('Saída do sistema (K)');
-legend('Alocação de Polos T_1', 'Alocação de Polos T_2', 'Alocação de Polos T_3','LQR T_1','LQR T_2','LQR T_3');
-title('Comparação entre Alocação de Polos e LQR');
-grid on;
-
-%%%% MALHA FECHADA %%%%
-
-disp(E); % Matriz de distúrbios
-
-
-% Verificando a controlabilidade
-Co = ctrb(sys);
-rank_co = rank(Co);
-
-if rank_co == size(A, 1)
-    disp('O sistema é controlável.')
-else
-    disp('O sistema não é controlável.')
+    Tsol_approx = [Tsol_approx T_approx_current];
+    Tsol = [Tsol T_current];
+    Tlqr = [Tlqr T_current_lqr];
+    Tmeasures = [Tmeasures T_measured];
 end
-
-% Verificando a observabilidade
-Ob = obsv(sys);
-rank_ob = rank(Ob);
-
-if rank_ob == size(A, 1)
-    disp('O sistema é observável.')
-else
-    disp('O sistema não é observável.')
-end
-%}
-%%%% ALOCACAO DE POLOS %%%%
-%{
-% Polos desejados
-desired_poles = [-1 + 1i, -1 - 1i , -0.0001];
-%TODO update based on LQR-search?
-
-
-% Calcular o ganho do controlador K
-Kp = place(A, B, desired_poles);
-disp('A matriz de ganhos K é:');
-disp(Kp);
-
-% Novo sistema com realimentação de estado
-A_cl = A - B * Kp;
-sys_cl = ss(A_cl, B, C, D);
-
-% Simular a resposta ao degrau do sistema
-t = 0:0.01:15;
-u = zeros(size(t));
-x0 = [T1; T2; T3];
-[yp, t, xp] = lsim(sys_cl, u, t, x0);
-
-% Plotar a resposta do sistema
 figure;
-plot(t, yp)
-xlabel('Tempo (s)')
-ylabel('Saída do sistema (K)')
-title('Resposta do Sistema com Alocação de Polos')
-legend('T_1','T_2','T_3')
-grid on
+plot(tSimulation,Tlqr(4,:));hold on
+plot(tSimulation,Tsol_approx(4,:),'--');hold on
+plot(tSimulation,Tsol(4,:));hold on
+legend("LQR full observe", "LQG approx", "LQG true")
+ylabel('T_4 (K)');
+xlabel('Time (s)');
+title("Added sensor noise and disturbances")
 
-% Número de entradas do sistema
-num_inputs = size(B1, 2);
-
-% Criar um vetor de entrada com o mesmo número de colunas que o número de entradas do sistema
-u1 = zeros(length(t), num_inputs);
-
-% Agora você pode usar este vetor de entrada com a função lsim
-[ynp, t, xnp] = lsim(sys, u1, t, x0);
-
-% Plotar a resposta do sistema
-figure;
-plot(t, ynp)
-xlabel('Tempo (s)')
-ylabel('Saída do sistema (K)')
-title('Resposta do Sistema sem Alocação de Polos')
-legend('T_1','T_2','T_3')
-grid on
-
-
-
-% Plotando os polos
-figure;
-pzmap(sys,'r',sys_cl, 'g',sys_cl1,'b');
-legend("Sem alocação", "Com alocação", "LQR")
-
-title('Mapa de Polos e Zeros');
-
-
-% Plot a step resonse 
-t = 0:0.1:20;
-
-respOpt = RespConfig;
-respOpt.InputOffset = [0];
-respOpt.Amplitude = [-2];
-respOpt.InitialState = [0,0,0,0,0,0];
-respOpt.Delay = 0;
 
 figure;
-step(sys,t,respOpt)
-title('Stepresponse to changed U_A, open loop');
+plot(tSimulation,Tmeasures(1,:),'Color',[0.2 0.2 0.9 0.2]);hold on
+plot(tSimulation,Tsol_approx(1,:),'--');hold on
+plot(tSimulation,Tsol(1,:));hold on
+legend("Measured", "LQG approx", "LQG true")
+ylabel('T_1 (K)');
+xlabel('Time (s)');
+title("Added sensor noise and disturbances")
 
-t = 0:0.1:20;
-figure;
-step(sys_cl1,t,respOpt)
-title('Stepresponse to changed U_A, LQR');
-ylabel('\Delta T compared to equlibrium (K)');
 
-
-%%
-%}
